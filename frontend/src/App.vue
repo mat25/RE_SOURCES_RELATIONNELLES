02835215a76adb7a9ae798bb319c972840a362d4
EpsithO <matheodu25@gmail.com>
2025-03-14 15:12:58 +0100

    parametrage du rooter

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,60 +5,15 @@
     <NavigationBar></NavigationBar>
   </Header>
 
+
+
   <main>
 
-    <h1>LES RESSOURCES DU JOUR :</h1>
-
-
-    <div class="cardContainer">
-
-      <n-card title="Card with Cover">
-        <template #cover>
-          <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
-        </template>
-        Card Content
-        <template #action>
-          <n-button type="info" >
-            Decouvrir
-          </n-button>
-        </template>
-      </n-card>
-
-
-      <n-card title="Card with Cover">
-        <template #cover>
-          <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
-        </template>
-        Card Content
-        <template #action>
-          <n-button type="info">
-            Decouvrir
-          </n-button>
-        </template>
-      </n-card>
-
-
-      <n-card title="Card with Cover">
-        <template #cover>
-          <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
-        </template>
-        Card Content
-        <template #action>
-          <n-button type="info">
-            Decouvrir
-          </n-button>
-        </template>
-      </n-card>
-      </div>
-
-    <n-button type="info" class="discover-all-btn">
-      Decouvrir toutes les ressources
-    </n-button>
+    <RouterView />
 
   </main>
 
 
-  <slot></slot>
   <Footer />
 
 </template>
@@ -85,63 +40,10 @@ export default defineComponent({
 
 <style scoped>
 
-.n-card {
-  max-width: 300px;
-  margin: 5rem auto;
-
-}
-
-.n-card :deep(.n-card__action) {
-  display: flex;
-  justify-content: center;
-}
-
-.discover-all-btn {
-  display: block;
-  margin: 0 auto;
-  padding: 10px 20px;
-  font-size: 1.2rem;
-  transform: translateY(-50%);
-}
-
-
-body{
-  background-color: rgba(13, 52, 158, 0.15);
-}
-
 main{
   width: 100%;
   height: 100vh;
   background-color: rgba(13, 52, 158, 0.15);
 }
-
-h1{
-  text-align: center;
-  color: rgba(13, 52, 158, 0.75);
-  transform: translateY(150%);
-}
-
-
-.allCards{
-  display: flex;
-  flex-direction: row;
-  margin: auto 2rem auto 2rem;
-}
-
-img{
-  border-bottom: 4px solid black;
-}
-
-.cardContainer{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  background-color: white;
-  height: 80vh;
-  width: 80vw;
-  margin: 0 auto;
-}
-
 </style>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import accueil from "@/views/accueil.vue";
 import profil from "@/views/profil.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path : '/',
+      path : '/profil',
       name: 'profil',
       component: profil,
+    },
+
+    {
+      path : '/',
+      name: 'accueil',
+      component: accueil,
     }
   ],
 })
