d2e557492c9d89def13f423a1a18d05f27f040f4
EpsithO <matheodu25@gmail.com>
2025-03-14 10:48:47 +0100

    Création du header et modification css de base

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,20 @@
 <template>
-    <h1>Hello word</h1>
+
+<Header/>
+
 </template>
 
-<script setup>
+<script >
+import { CashOutline as CashIcon } from '@vicons/ionicons5'
+import { defineComponent } from 'vue'
+import Header from "@/components/template/Header.vue";
+import NavigationBar from "@/components/organisms/NavigationBar.vue";
 
+export default defineComponent({
+  components: {
+    Header,
+    NavigationBar,
+    CashIcon
+  }
+})
 </script>
-
-<style scoped>
-
-</style>
---
+++ frontend/src/components/template/Header.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <header class="header">
+    <img src="~@/assets/9464471.png" alt="">
+    <h1 class="title">Ressources Relationnel</h1>
+  </header>
+
+</template>
+
+<style scoped>
+
+    *{
+      font-family: "Apple SD Gothic Neo", cursive;
+    }
+
+    .header{
+      width: 100%;
+      height: 7rem;
+      background-color: white;
+      display: flex;
+    }
+
+    .title{
+      color: #222222;
+      padding: 2rem;
+      font-weight: bold;
+      font-family: "Apple SD Gothic Neo", cursive;
+    }
+
+    img{
+      width: 5rem;
+      height: 5rem;
+      margin-left: 1rem;
+      margin-top: 1rem;
+    }
+
+
+</style>
