4b12f99924a00e4181418981a05afb89e6596e12
matheo.maire <matheodu25@gmail.com>
2025-03-18 16:09:51 +0100

    to do list pour chaque pages

---
+++ frontend/src/views/detailResources.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+
+</style>
+
+
+
+
+// avoir un bouton favoris
+// avoir un bouton deroulant pour choisir si la ressources est consulter ou non 
+// ajouter un espaces commentaire
+// ajouter des commentaire sur l'espaces
+// ajouter une etoiles sur le commentaire du createurs de la ressources
+// modifier ca ressources
+// partage d'une ressources
+// faire une chatbox ou les utilisateur peuvent parler entre eux
---
+++ frontend/src/views/progression.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+
+</style>
+
+
+
+// faire des statistique sur le nombr3e de ressources favoris, mis de exploiter ou non, 
+// annuler ou mettre de coter une ressources de jeux
+// le nombre de messages et de commentaire poster ou envoyer 
